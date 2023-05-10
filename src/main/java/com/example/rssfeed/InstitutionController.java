0393349f725f62bdccb011c2fec666ac6fd46aa7
prasisiri <prasanna.iitr@gmail.com>
2023-05-10 18:45:47 -0500

    redis changes

--- src/main/java/com/example/rssfeed/InstitutionController.java
+++ src/main/java/com/example/rssfeed/InstitutionController.java
@@ -2,16 +2,48 @@ package com.example.rssfeed;
 
 import java.util.ArrayList;
 import java.util.List;
-
+import java.util.Set;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.json.JSONObject;
 import org.json.JSONArray;
 import java.util.UUID;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 public class InstitutionController {
+
+    @Autowired
+    private RedisTemplate<String, Institution> redisTemplate;
+
+    private static final String INSTITUTIONS_CACHE_PREFIX = "institutions:";
+
+    private Institution getCachedInstitutionByGuid(String guid) {
+        Set<String> keys = redisTemplate.keys(INSTITUTIONS_CACHE_PREFIX + "*" + guid);
+        if (keys == null || keys.isEmpty()) {
+            return null;
+        }
+        String key = keys.iterator().next();
+        Institution institution = redisTemplate.opsForValue().get(key);
+        return institution;
+    }
+
+    @GetMapping("/institutions/by-guid")
+    public ResponseEntity<Institution> getInstitutionByGuid(@RequestParam String guid) {
+    Institution institution = getCachedInstitutionByGuid(guid);
+    if (institution == null) {
+        return ResponseEntity.notFound().build();
+    }
+    return ResponseEntity.ok(institution);
+}
+
+    
 
     @GetMapping("/institutions")
     public List<Institution> getInstitutions() {
@@ -34,20 +66,39 @@ public class InstitutionController {
             institution.setUrl("http://localhost:3005");
             institutions.add(institution);
         }
+        // Cache institutions in Redis
+        cacheInstitutions(institutions);
+
         return institutions;
     }
 
     @GetMapping("/institutions/typeahead")
     public List<Institution> getInstitutionsBySearchQuery(@RequestParam String query) {
-        List<Institution> matchingInstitutions = new ArrayList<>();
-        List<Institution> institutions = getInstitutions();
-        for (Institution inst : institutions) {
-            if (inst.getName().toLowerCase().contains(query.toLowerCase())) {
-                matchingInstitutions.add(inst);
-            }
-        }
+        List<Institution> matchingInstitutions = getCachedInstitutionsByName(query);
         return matchingInstitutions;
     }
+    
+    
+
+    private void cacheInstitutions(List<Institution> institutions) {
+        ValueOperations<String, Institution> valueOperations = redisTemplate.opsForValue();
+        for (Institution institution : institutions) {
+            String key = INSTITUTIONS_CACHE_PREFIX + institution.getName().toLowerCase() + ":" + institution.getGuid();
+            valueOperations.set(key, institution, 24, TimeUnit.HOURS); // Cache for 24 hours
+        }
+    }
+
+    private List<Institution> getCachedInstitutionsByName(String name) {
+        Set<String> keys = redisTemplate.keys(INSTITUTIONS_CACHE_PREFIX + name.toLowerCase() + "*");
+        if (keys == null || keys.isEmpty()) {
+            return new ArrayList<>();
+        }
+        List<Institution> institutions = redisTemplate.opsForValue().multiGet(keys);
+        return institutions;
+    }
+    
+    
+    
     
 }
 
---
+++ src/main/java/com/example/rssfeed/RedisConfiguration.java
@@ -0,0 +1,22 @@
+package com.example.rssfeed;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfiguration {
+
+    @Bean
+    public RedisTemplate<String, Institution> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Institution> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        return template;
+    }
+
+}
